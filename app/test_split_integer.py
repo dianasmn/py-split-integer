38c6d1022befffd74505a4cbbdf1d7d110e94806
Diana Semenyuta <diana@example.com>
2026-02-25 00:14:21 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,38 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    result = split_integer(8, 1)
+    assert sum(result) == 8
+    assert len(result) == 1
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    value = 8
+    parts = 2
+    expected_part = value // parts
+    assert split_integer(value, parts) == [expected_part] * parts
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    value = 8
+    assert split_integer(value, 1) == [value]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    result = split_integer(17, 4)
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    value = 2
+    number_of_parts = 5
+    result = split_integer(value, number_of_parts)
+    assert len(result) == number_of_parts
+    assert sum(result) == value
+    assert result == sorted(result)
+    assert min(result) == 0
+
+
+def test_difference_between_max_and_min_should_be_at_most_one() -> None:
+    result = split_integer(2, 5)
+    assert max(result) - min(result) <= 1
